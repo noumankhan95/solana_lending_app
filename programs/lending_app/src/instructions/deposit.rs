5fc52a133d0cffba6d43191de2b74c61d98deb7e
noumankhan95 <noumankhan95@yahoo.com>
2025-10-27 22:47:02 +0500

    adding logic for deposit

---
+++ programs/lending_app/src/instructions/deposit.rs
@@ -0,0 +1,31 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+use crate::states::{Bank, User};
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(mut,seeds=[mint.key().as_ref()],bump)]
+    pub bank: Account<'info, Bank>,
+
+    #[account(mut,seeds=[b"treasury",mint.key().as_ref()],bump)]
+    pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(mut,seeds=[signer.key().as_ref()],bump)]
+    pub user_account: Account<'info, User>,
+
+    #[account(mut,associated_token::mint=mint,associated_token::authority=signer,associated_token::token_program=token_program)]
+    pub user_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
